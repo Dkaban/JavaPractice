f14583ef48bb7a90923f0e2aa4d79e1f358764e3
Dustin <dustinkaban@gmail.com>
2020-06-09 11:28:25 -0700

    Update OutputFormat.java

--- OutputFormat.java
+++ OutputFormat.java
@@ -4,7 +4,7 @@
 // Format output so that it has 15 spaces between the integer and the string and if the string is < 10 we add 2 0's
 // If int 10 < x < 100 we add 1 0.
 // Solution Created By: Dustin Kaban
-// Date: June 5th, 2020
+// Date: June 9th, 2020
 // ***************************
 
 import java.util.Scanner;
