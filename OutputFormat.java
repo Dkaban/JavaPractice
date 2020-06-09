9c1491bf85e9400caa7a0c3295b4f9328f0b0fcf
Dustin <dustinkaban@gmail.com>
2020-06-09 11:28:11 -0700

    Create OutputFormat.java

---
+++ OutputFormat.java
@@ -0,0 +1,25 @@
+  
+// THE PROBLEM
+// ***************************
+// Format output so that it has 15 spaces between the integer and the string and if the string is < 10 we add 2 0's
+// If int 10 < x < 100 we add 1 0.
+// Solution Created By: Dustin Kaban
+// Date: June 5th, 2020
+// ***************************
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s%03d%n",s1,x);
+            }
+            System.out.println("================================");
+
+    }
+}
